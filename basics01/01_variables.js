c1d43675af83c7a54ae894c1771051064e362092
KSHIVA339 <kshiva12493@gmail.com>
2024-08-04 09:10:12 +0000

    variables added

---
+++ basics01/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =339
+let accountEmail ="shiva@google.com"
+var accountPassword ="12345"
+accountCity ="Anantapur"
+let accountState;
+
+//  accountId = 5 
+
+console.log(accountId);
+// accountId =58
+
+accountEmail ="ksk@google.com"
+accountPassword = "79945"
+accountCity = "DMM"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
