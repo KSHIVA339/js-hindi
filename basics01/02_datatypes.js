a54dc2ac418cbbf43bf66a67621b7258c94d76ff
KSHIVA339 <kshiva12493@gmail.com>
2024-08-04 09:38:56 +0000

    datatypes

---
+++ basics01/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+// alert (3+5) // we are using node.js not engine browser
+
+let age = 18
+let name ="jawan"
+let city = "hyderabad"
+
+console.log(3+10)
+ console.log ("Jeevan dan")
+
+/* number => 2 ti pwer 53
+ bigint 
+ string => ""
+ boolean => true/false
+ null => standalone value
+undefined => value not assigned
+sysmbol => unique  (primtive data types)
+object => 
+    */
+
+
+console.log(typeof age);
